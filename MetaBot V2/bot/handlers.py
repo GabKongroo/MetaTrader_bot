40e016f636cdf64746656b01c8cab7ff8f8f2b39
Gabbuz <120514689+GabKongroo@users.noreply.github.com>
2025-01-05 16:36:27 +0100

    Delete MetaBot V2 directory

--- MetaBot V2/bot/handlers.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import CallbackQueryHandler, CommandHandler
-from bot.commands import start
-from bot.callbacks import handle_menu_callback
-
-def setup_handlers(application):
-    """Configura gli handler per il bot."""
-    # Handler per il callback 'register_or_login'
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(handle_menu_callback))
